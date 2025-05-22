78894777bad6f01c6fdec6bd9023a7186c4fc18c
Sebastian Siatka <siatkasebastian@gmail.com>
2025-05-22 17:42:31 +0200

    Fetching addresses and shops list comp. usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+import "./App.css";
+
+import { ShopsList } from "./components/ShopsList";
+import { fetchShopsAddresses, type ShopDTO } from "./api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [addresses, setAddresses] = React.useState<ShopDTO[]>([]);
+
+  React.useEffect(() => {
+    getShopsAddresses();
+  }, []);
+
+  const getShopsAddresses = async () => {
+    try {
+      const res = await fetchShopsAddresses();
+
+      setAddresses(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ShopsList shopsAddresses={addresses} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
